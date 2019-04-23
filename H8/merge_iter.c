ed26e07613f89f97ccdb58b353092173dabf7f38
imtiaz <imtiazkhaled07@gmail.com>
2019-04-22 22:16:56 -0500

    fixed small bugs, and started on functionality

--- H8/merge_iter.c
+++ H8/merge_iter.c
@@ -1,27 +1,42 @@
 #include <stdio.h>
-
-void mergeSort(int * arrayInt, arraySize)   {
-    printArray(arrayInt, arraySize);
-    for(i = 0; i < arraySize; i++)    arrayInt[i] = -1;
-    printArray(arrayInt, arraySize);
-    
-}
+#include <stdlib.h>
 
 void printArray(int * currentArray, int currentSize)   {
     int i;
-    for(i = 0; i < currentSize; i++)    printf("%5d",currentArray[i]);
+    for(i = 0; i < currentSize; i++)    printf("%5d,",currentArray[i]);
     printf("\n");
 }
 
 
+
+void mergeSort(int * arrayInt, int arraySize)   {
+    int i;
+    int currnetSize, leftIndex;
+    printArray(arrayInt, arraySize);
+    //for(i = 0; i < arraySize; i++)    arrayInt[i] = -1;
+    for(currentSize = 1; currentSize < arraySize - 1; currentSize *= 2)  {
+        for(leftIndex = 0; leftIndex < arraySize - 1; leftIndex += currentSize) {
+                int mid = ( leftIndex + currentSize ) / 2;
+                int right = ( ( currentSize - 1 ) - leftIndex );
+
+        }
+    }  
+    printArray(arrayInt, arraySize);
+    
+}
+
 int main(void)  {
+    int arraySize; 
     do
     {
-        int arraySize;
+        int i;
         scanf("%d",&arraySize);
-        int * arrayInt = malloc(arraySize*sizeof(int));
-        mergeSort(arrayInt, arrarySize);
+        int * arrayInt = malloc(arraySize*sizeof(int) * arraySize);
+        for(i = 0; i < arraySize; i++)  scanf("%d",&arrayInt[i]);
+        printf("\n");
+        mergeSort(arrayInt, arraySize);
     } while (arraySize != -1);
+    free(arrayInt);
     return 0;    
 }
 
