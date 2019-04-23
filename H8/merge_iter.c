d23cec7a877fdaf107005bab82cb8eb50ef83866
imtiaz <imtiazkhaled07@gmail.com>
2019-04-22 21:09:56 -0500

    test to if function setup is correct

--- H8/merge_iter.c
+++ H8/merge_iter.c
@@ -1,11 +1,27 @@
 #include <stdio.h>
 
+void mergeSort(int * arrayInt, arraySize)   {
+    printArray(arrayInt, arraySize);
+    for(i = 0; i < arraySize; i++)    arrayInt[i] = -1;
+    printArray(arrayInt, arraySize);
+    
+}
+
+void printArray(int * currentArray, int currentSize)   {
+    int i;
+    for(i = 0; i < currentSize; i++)    printf("%5d",currentArray[i]);
+    printf("\n");
+}
+
+
 int main(void)  {
     do
     {
         int arraySize;
         scanf("%d",&arraySize);
         int * arrayInt = malloc(arraySize*sizeof(int));
+        mergeSort(arrayInt, arrarySize);
     } while (arraySize != -1);
-    
+    return 0;    
 }
+
