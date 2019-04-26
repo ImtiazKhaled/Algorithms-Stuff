c00767fa9231828437691451055191ed90fdf818
imtiaz <imtiazkhaled07@gmail.com>
2019-04-26 18:38:59 -0500

    merge runs, but with bugs

--- H8/merge_iter.c
+++ H8/merge_iter.c
@@ -10,10 +10,25 @@ void printArray(int * currentArray, int currentSize)   {
 
 
 void merge(int * arrayInt, int leftIndex, int midIndex, int rightIndex) {
-    int i, j, k, sizeOne = (midIndex - leftIndex + 1), sizeTwo = (rightIndex - midIndex);
+    int i, j, k;
+    int sizeOne = (midIndex - leftIndex + 1);
+    int sizeTwo = (rightIndex - midIndex);
     int arrayLeft[sizeOne], arrayRight[sizeTwo];
-    for(i = 0; i < sizeOne; ++i)
-
+    for(i = 0; i < sizeOne; ++i)    arrayLeft[i] = arrayInt[ leftIndex + i ];
+    for(j = 0; i < sizeTwo; ++j)    arrayRight[j] = arrayInt[ rightIndex + j ];
+    i = 0, j = 0, k = 0;
+    
+    for( k =  leftIndex; k < rightIndex; ++k)   {
+        if(arrayLeft[i] <= arrayRight[j])    {
+            arrayInt[k] = arrayLeft[i];
+            ++i;
+        }else
+        {
+            arrayInt[k] = arrayRight[j];
+            ++j;
+        }
+        
+    }
     return;
 }
 
@@ -22,12 +37,14 @@ void mergeSort(int * arrayInt, int arraySize)   {
     int i;
     int currentSize, leftIndex;
     printArray(arrayInt, arraySize);
-    for(currentSize = 1; currentSize <= arraySize - 1; currentSize = currentSize*2)  {
-        for(leftIndex = 0; leftIndex < arraySize - 1; leftIndex = currentSize*2) {
+    for(currentSize = 1; currentSize <= arraySize - 1; currentSize *= 2)  {
+        for(leftIndex = 0; leftIndex < arraySize - 1; leftIndex += 2*currentSize) {
                 int midIndex = leftIndex + currentSize - 1;
-                int rightIndex = minimum( leftIndex + 2*currentSize -1 , arraySize - 1 );
+                int potentialRight = (leftIndex - 1) + 2*currentSize;
+                int otherPotentialRight = arraySize - 1;
+                int rightIndex = minimum( potentialRight , otherPotentialRight );
                 merge(arrayInt, leftIndex, midIndex, rightIndex );
-
+                //printf("left: %d, mid: %d, right: %d, current size: %d\n",leftIndex,midIndex,rightIndex,currentSize);      
         }
     }  
     printArray(arrayInt, arraySize);
@@ -35,10 +52,10 @@ void mergeSort(int * arrayInt, int arraySize)   {
 }
 
 int main(void)  {
-    int arraySize; 
+    int arraySize;
+
     do
     {
-        
         int i;
         scanf("%d",&arraySize);
         int * arrayInt = malloc(arraySize*sizeof(int) * arraySize);
@@ -46,7 +63,6 @@ int main(void)  {
         printf("\n");
         mergeSort(arrayInt, arraySize);
         free(arrayInt); 
-
     } while (arraySize != -1);
     return 0;    
 }
